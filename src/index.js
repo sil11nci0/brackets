2259ef5b3c9d4bec86af0e40ef48bdaed201ce4c
Aliaksei <alexey.alexandrovich7@gmail.com>
2024-08-14 00:05:50 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+    const openBrackets = {};
+    const closeBrackets = {};
+
+    for (const [open, close] of bracketsConfig) {
+        openBrackets[open] = close;
+        closeBrackets[close] = open;
+    }
+    for (let char of str) {
+        if (openBrackets[char]) {
+            if (char === openBrackets[char] && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (closeBrackets[char]) {
+            if (stack.length === 0 || stack.pop() !== closeBrackets[char]) {
+                return false; 
+            }
+        }
+    }
+    return stack.length === 0;
 }
